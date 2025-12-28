17c7063eb414c095f14ecba5d2f25e20ef4df214
Corey Ames <ctomames@gmail.com>
2025-12-28 01:16:48 -0500

    fixed proj shooting from end of gun

--- gun.gd
+++ gun.gd
@@ -17,7 +17,7 @@ func use(direction: Vector3) -> void:
 	var projectile: Proj = ammo_scene.instantiate();
 	projectile.position = global_position;
 	projectile.rotation = global_rotation;
-	projectile.translate_object_local(Vector3(0, PROJ_SPAWN_Y_OFFSET, -PROJ_SPAWN_Z_OFFSET));
+	projectile.translate_object_local(Vector3(0, PROJ_SPAWN_Y_OFFSET, PROJ_SPAWN_Z_OFFSET));
 	get_node("/root").add_child(projectile);
 	$AnimationPlayer.play("use");
 	projectile.fire(direction); 
