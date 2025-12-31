81b179cb6abd41e275d4902cbf4472a4cf311ff1
Corey Ames <ctomames@gmail.com>
2025-12-31 00:46:46 -0500

    fixed proj traveling with gravity, added optional stdout print to debug log

--- debug_msg_bus.gd
+++ debug_msg_bus.gd
@@ -5,10 +5,13 @@ signal toggle_debug(on: bool)
 
 const debug_scene: Resource = preload("res://debug_hud.tscn");
 var is_on: bool = false;
+var print_to_stdout: bool = true;
 
 func log(msg: String) -> void:
 	if is_on:
 		emit_signal("msg_ready", msg);
+	if print_to_stdout:
+		print(msg);
 	return;
 	
 func toggle() -> bool:
--- proj.gd
+++ proj.gd
@@ -1,8 +1,7 @@
 class_name Proj
 extends AnimatableBody3D
 
-# 10 is really fast
-const SPEED = 5;
+const SPEED = 60;
 
 var direction: Vector3;
 var motion: Vector3;
@@ -18,25 +17,22 @@ var status: Status = Status.LOADED;
 
 func _ready() -> void:
 	$CollisionShape3D.scale = Vector3(.05, .05, .05);
-	
 	return;
 
 func _process(delta: float) -> void:
 	if (status == Status.FIRE):
 		status = Status.TRAVELING;
-		motion = motion * delta;
 	elif (status == Status.TRAVELING):
-		motion += get_gravity() * delta;
-		print(motion);
-		handle_collision(move_and_collide(motion));
+		motion += Vector3(0, get_gravity().y * delta *.001 , 0);
+	elif (status == Status.DONE):
+		queue_free();
+		return;
+	handle_collision(move_and_collide(motion));
 	return
-
-func _physics_process(delta: float) -> void:
-	return;
 
 func fire(_direction: Vector3) -> void:
 	direction = _direction;
-	motion = direction;
+	motion = direction * get_process_delta_time() * SPEED;
 	status = Status.FIRE;
 	Debug.log("fired proj");
 	return;
@@ -44,9 +40,8 @@ func fire(_direction: Vector3) -> void:
 func handle_collision(collision: KinematicCollision3D) -> bool:
 	if collision == null:
 		return false;
-	print(collision.get_collider_id());
+	Debug.log(str(collision.get_collider_id()));
 	status = Status.DONE;
-	queue_free();
 	return true;
 	
 	
