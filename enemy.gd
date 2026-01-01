0e42e3e8989b528fcf6bfbf5f3f231ed4b7da7e5
corey <ctomames@gmail,com>
2026-01-01 00:31:57 -0500

    update

--- enemy.gd
+++ enemy.gd
@@ -8,30 +8,29 @@ var chance_to_change = .5;
 var chance_to_jump = .3;	
 var dir_entropy: float = 0;
 var jump_entropy: float = 0;	
-var current_dir: StringName = "ui_up";
-var change_dir: bool = false;
+var current_action: StringName = "ui_up";
 var to_jump: bool = false
 
 func _ready() -> void:
-	current_dir = dir_actions.pick_random();
+	current_action = dir_actions.pick_random();
 	return;
 
-func generate_input() -> void:
+func generate_input(delta: float) -> void:
 	var dir_chance = chance_to_change * dir_entropy;
 	var dir_roll = randf();
 	if dir_roll > 1 - dir_chance:
-		change_dir = true;
+		current_action = dir_actions.pick_random();
+		dir_entropy = 0;
+	else:
+		dir_entropy += .001;
 		
-		
-	
-			
-	
-	
-	
-	# entropy affects chance for action
-	# previous action affects next action chance
-	#  	for instance, switching direction will reset entropy, making staying direction more likely at first
-	# 
+	var jump_chance = chance_to_jump * jump_entropy;
+	var jump_roll = randf();
+	if jump_roll > 1 - jump_chance:
+		to_jump = true;
+		jump_entropy = 0;
+	else:
+		jump_entropy += .001;
 	return;
 
 func _physics_process(delta: float) -> void:
@@ -45,7 +44,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down");
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
