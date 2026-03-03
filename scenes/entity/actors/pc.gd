2eb0dc6007049f4263c24390a4b7c17b602260f6
Corey Ames <ctomames@gmail.com>
2026-03-02 23:30:42 -0500

    messing with air physics; disabled control after jump, added air decel

--- scenes/entity/actors/pc.gd
+++ scenes/entity/actors/pc.gd
@@ -6,6 +6,8 @@ signal was_hit
 
 const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
+const AIR_DECEL_START: float = 1.0/(SPEED*SPEED*10*10)
+var decel: float = SPEED
 var is_ready_jump: bool = true
 var is_directed: bool = false
 var was_airborne: bool = false
@@ -72,19 +74,22 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("use") && equipped != null:
 		equipped.use(get_viewport().get_camera_3d().project_ray_normal(Vector2(1920.0/2, 1080.0/2)))
 
-	if direction:
+	if direction && is_on_floor():
 		if !is_directed:
 			is_directed = true
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		if is_on_floor():
-			walking_sound(true)
+		walking_sound(true)
 	else:
 		if is_directed:
 			is_directed = false
 			walking_sound(false)
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		if is_on_floor():
+			decel = SPEED
+		else:
+			decel = AIR_DECEL_START
+		velocity.x = move_toward(velocity.x, 0, decel)
+		velocity.z = move_toward(velocity.z, 0, decel)
 		
 	if (move_and_slide()):
 		handle_collisions()
@@ -102,7 +107,6 @@ func _input(event: InputEvent) -> void:
 			remove_child(console_node)
 			is_console_open = false
 			return
-			
 	else:
 		if event is InputEventKey:
 			if event.is_action_pressed('cancel'):
