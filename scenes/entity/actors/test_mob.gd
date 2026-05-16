6820b3041a2751fe10e8423368c14accda1a311d
Corey Ames <ctomames@gmail.com>
2026-05-16 10:26:15 -0400

    working on test setup for mob movement

--- scenes/entity/actors/test_mob.gd
+++ scenes/entity/actors/test_mob.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	super._ready()
 	# try to fake up player node if not present on test map
 	if !player_node:
-		player_node = world.get_node_or_null("SM_LittleHouse")
-		if player_node:
-			player_location = player_node.global_position
+		var house_node = world.get_node_or_null("SM_LittleHouse")
+		if house_node:
+			player_location = house_node.global_position
 	return
--- scenes/environment/iso_ai_test.gd
+++ scenes/environment/iso_ai_test.gd
@@ -4,8 +4,11 @@ var level_collision_id: int
 
 func _ready() -> void:
 	level_collision_id = $Level.get_instance_id()
+	$Level.connect("input_event", _on_level_click)
 	return
 
-func _input(event: InputEvent) -> void:
-	#if Input.is_action_just_pressed("waypoint"):	
-	return	
+func _on_level_click(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton && Input.is_action_just_pressed("use"):
+		print(event_position)
+		$Mob.player_location = event_position
+	return 
